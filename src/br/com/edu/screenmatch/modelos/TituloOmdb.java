098980dcb4d3f1a0a941445730037b1418f9c842
Eduardo Morais <edumorais08@gmail.com>
2024-05-23 02:07:49 -0300

    transformando json em classe

---
+++ src/br/com/edu/screenmatch/modelos/TituloOmdb.java
@@ -0,0 +1,4 @@
+package br.com.edu.screenmatch.modelos;
+
+public record TituloOmdb(String title, String year, String runtime) {
+}
--- src/br/com/edu/screenmatch/principal/PrincipalComBusca.java
+++ src/br/com/edu/screenmatch/principal/PrincipalComBusca.java
@@ -1,6 +1,11 @@
 package br.com.edu.screenmatch.principal;
 
 import br.com.edu.screenmatch.modelos.Filme;
+import br.com.edu.screenmatch.modelos.Titulo;
+import br.com.edu.screenmatch.modelos.TituloOmdb;
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 import java.io.IOException;
 import java.net.URI;
@@ -18,7 +23,6 @@ public class PrincipalComBusca {
 
         seuFilme = seuFilme.toLowerCase();
         seuFilme = seuFilme.replaceAll(" ", "+");
-        System.out.println(seuFilme);
 
         String chave = "https://www.omdbapi.com/?t=" + seuFilme + "&apikey=4ce2fad8";
 
@@ -31,6 +35,20 @@ public class PrincipalComBusca {
         HttpResponse<String> response = client
                 .send(request, HttpResponse.BodyHandlers.ofString());
 
-        System.out.println(response.body());
+        String json = response.body();
+        System.out.println(json);
+
+        Gson gson = new GsonBuilder()
+                .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
+                .create();
+//      Titulo meuTitulo = gson.fromJson(json, Titulo.class);
+        TituloOmdb meuTituloOmdb = gson.fromJson(json, TituloOmdb.class);
+
+
+        System.out.println(meuTituloOmdb);
+        Titulo meuTitulo = new Titulo(meuTituloOmdb);
+        System.out.println("HAHAHAHA");
+        System.out.println(meuTitulo);
+
     }
 }
